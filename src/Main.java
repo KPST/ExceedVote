cc6277cba633eba4928258a28f36d8d8eb0e8c08
Kunat Pipatanakul <kunato36@gmail.com>
2012-10-02 16:37:07 +0700

    v2 update

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Client client = new Client();
+		UI ui = new UI();
+		ClientController c = new ClientController(client,ui);
+		
+		c.run();
+	}
+
+}
